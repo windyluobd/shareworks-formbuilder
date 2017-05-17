10bee7d965aabc18ecb0cd06e685c8c5d8dc2358
windy.luo <windy.luo@shareworks.cn>
2017-05-17 15:09:33 +0800

    init

---
+++ src/js/events.js
@@ -0,0 +1,21 @@
+/**
+ * Form Builder events
+ * @return {Object} various events to be trigger
+ */
+// function fbEvents(){
+  const events = {};
+
+  events.loaded = new Event('loaded');
+  events.viewData = new Event('viewData');
+  events.userDeclined = new Event('userDeclined');
+  events.modalClosed = new Event('modalClosed');
+  events.modalOpened = new Event('modalOpened');
+  events.formSaved = new Event('formSaved');
+  events.fieldAdded = new Event('fieldAdded');
+  events.fieldRemoved = new Event('fieldRemoved');
+  events.fieldRendered = new Event('fieldRendered');
+
+//   return events;
+// }
+
+export default events;
